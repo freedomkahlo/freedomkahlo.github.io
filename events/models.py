df87a1da68d24fa080a6387111b5ba6976300bd2
kingroy <roy_h_zhao@yahoo.com>
2015-04-06 10:37:43 -0400

    Fixed Compatibility Issues (Some Still Remain!!)
    Now parses dates and validates that events are correctly formatted.
    BUT-HTML CURRENTLY HAS NO WAY TO HANDLE VALIDATION ERROR-NEED TO ADD
    THAT

--- events/models.py
+++ events/models.py
@@ -15,17 +15,19 @@ class Instance(models.Model):
 	def clean(self):
 		if len(self.title.replace(' ', '')) == 0:
 			raise ValidationError('Title cannot be left blank.')
-		if self.desc == None:
-			self.desc = ' '
-		startd = datetime.combine(self.start_date, self.start_time)
-		endd = datetime.combine(self.end_date, self.end_time)
+		if self.start_date == '' or self.end_date == '':
+			raise ValidationError('Dates cannot be left blank.')
+		if self.start_time == '' or self.end_time == '':
+			raise ValidationError('Times cannot be left blank.')
+		startd = datetime.strptime(self.start_date + ' ' + self.start_time, '%Y-%m-%d %H:%M')
+		endd = datetime.strptime(self.end_date + ' ' + self.end_time, '%Y-%m-%d %H:%M')
 		if (startd >= endd):
-			raise ValidationError('Start date must occur before end time.')
+			raise ValidationError('Start time must occur before end time.')
 		if (startd < datetime.now() - timedelta(0, 60)):
 			raise ValidationError('Start date must occur in the future.')
 		self.pub_date = timezone.now()
 	def __str__(self):
-		return self.title + "\n" + str(self.start_date) + ", " + str(self.start_time) + " to " + str(self.end_date) + ", " + str(self.end_time)
+		return self.title + "\n" + self.start_date + ", " + self.start_time + " to " + self.end_date + ", " + self.end_time
 	def save(self, **kwargs):
 		self.clean()
 		return super(Instance, self).save(**kwargs)
