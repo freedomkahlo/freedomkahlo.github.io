0e9414b056ffb6cb782b224ec359bb617190bcf0
minseungchoi <minsung0315@gmail.com>
2015-04-12 03:33:28 -0400

    asdff

--- backend/cal.py
+++ backend/cal.py
@@ -78,9 +78,9 @@ def auth(request):
 	authcode = request.GET['code']
 	print authcode
 
-	post_data = [('code',authcode), ('client_id',CLIENT_SECRETS_JSON['client_id']), ('client_secret',CLIENT_SECRETS_JSON['client_secret']), ('redirect_uri','http://skedg.tk/'), ('grant_type','authorization_code'),]
-	print urllib.urlencode(post_data)
-	result = urllib2.urlopen('https://www.googleapis.com/oauth2/v3/token', urllib.urlencode(post_data))
+	post_data = {'code':authcode, 'client_id':CLIENT_SECRETS_JSON['client_id'], 'client_secret':CLIENT_SECRETS_JSON['client_secret'], 'redirect_uri':'http://skedg.tk/', 'grant_type':'authorization_code'}
+	#print urllib.urlencode(post_data)
+	result = requests.post('https://www.googleapis.com/oauth2/v3/token', data=post_data)
 	content = result.read()
 	print content
 	return HttpResponseRedirect('/events/')
