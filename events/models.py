8721f33e8597c5597f70150239b9a35f1af87b7c
kingroy <roy_h_zhao@yahoo.com>
2015-04-16 14:28:15 -0400

    Fixed Time Error

--- events/models.py
+++ events/models.py
@@ -22,8 +22,8 @@ class Instance(models.Model):
 			raise ValidationError('Dates cannot be left blank.')
 		if self.start_time == '' or self.end_time == '':
 			raise ValidationError('Times cannot be left blank.')
-		startd = datetime.strptime(self.start_date + ' ' + self.start_time, '%m/%d/%Y %H:%M %p')
-		endd = datetime.strptime(self.end_date + ' ' + self.end_time, '%m/%d/%Y %H:%M %p')
+		startd = datetime.strptime(self.start_date + ' ' + self.start_time, '%m/%d/%Y %I:%M %p')
+		endd = datetime.strptime(self.end_date + ' ' + self.end_time, '%m/%d/%Y %I:%M %p')
 		if (startd >= endd):
 			raise ValidationError('Start time must occur before end time.')
 		if (startd < datetime.now() - timedelta(0, 60)):
