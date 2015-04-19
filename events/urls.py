c50f1cb0fb407d8a9475ec5c6375ec343966eaae
jl25 <jonathanlin@nat-oitwireless-inside-vapornet100-a-31543.Princeton.EDU>
2015-04-19 17:42:44 -0400

    Got rid of delete invitee

--- events/urls.py
+++ events/urls.py
@@ -6,7 +6,6 @@ from backend import cal
 urlpatterns = [
 	url(r'^(?P<instance_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^add/', views.add, name='add'),
-	url(r'^deleteInvitee/', views.deleteInvitee, name='deleteInvitee'),
 	url(r'^manageCreator/', views.manageCreator, name='manageCreator'),
 	url(r'^manageInvitee/', views.manageInvitee, name='manageInvitee'),
 	url(r'^manageNotification/', views.manageNotification, name='manageNotification'),
