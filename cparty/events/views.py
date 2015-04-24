9fcb11095189c55b904b3607520a90819a62813e
cjqian <crystaljqian@gmail.com>
2015-04-24 11:02:28 -0400

    hi
    hi

--- cparty/events/views.py
+++ cparty/events/views.py
@@ -42,7 +42,7 @@ def add(request):
 	end_date=request.POST.get('end_date', '')
 	time_range=request.POST.get('time_range', '')
 	event_length=request.POST.get('event_length', '')
-	creator=request.POST.get('creator', '')
+	creator=request.POST.get['creator']
 
 	latest_event_list = Instance.objects.order_by('-pub_date')[:100]
 	returnMsg = {'error': '', 'latest_event_list': latest_event_list,
