f9d63464eafc0f2a7f79c8d59fd18701b296b006
cjqian <crystaljqian@gmail.com>
2015-04-28 12:12:32 -0400

    ugh uhg who even put this in creator

--- cparty/events/views.py
+++ cparty/events/views.py
@@ -109,18 +109,10 @@ def delete(request):
 
 	event.delete()
 	return HttpResponseRedirect('/events/')
-
-def roundUpByTimeDelta(dt, roundTo = roundToMin * 60):
-	"""Round a datetime object to any time laps in seconds
-	dt : datetime.datetime object, default now.
-	roundTo : Closest number of seconds to round up to, default 15 minutes.
-	"""
-	seconds = (dt - dt.min).seconds
-	# // is a floor division, not a comment on following line:
-	rounding = (seconds+roundTo) // roundTo * roundTo
-	return timedelta(0,rounding-seconds,-dt.microsecond)
 		
 def getTimes(request):
+	roundToMin = 15 #minutes
+
 	def roundUpByTimeDelta(dt, roundTo = roundToMin * 60):
 		"""Round a datetime object to any time laps in seconds
 		dt : datetime.datetime object, default now.
@@ -131,7 +123,7 @@ def getTimes(request):
 		rounding = (seconds+roundTo) // roundTo * roundTo
 		return timedelta(0,rounding-seconds,-dt.microsecond)
 
-	roundToMin = 15 #minutes
+	
 
 	eventID = request.POST['eventID']
 	event = get_object_or_404(Instance, eventID=eventID)
