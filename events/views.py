e7cc61ed59e4e7504bc89d575cb19cd1ca88a3e2
jl25 <jonathanlin@nat-oitwireless-inside-vapornet100-a-31543.Princeton.EDU>
2015-05-04 17:25:17 -0400

    s

--- events/views.py
+++ events/views.py
@@ -550,8 +550,8 @@ def register_confirm(request, activation_key):
 
 @login_required
 def userPage(request):
-
-	return render(request, '/events/user.html')
+	
+	return render(request, 'events/user.html')
 
 def user_login(request):
 	context = RequestContext(request)
