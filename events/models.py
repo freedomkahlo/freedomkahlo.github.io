89fffe7a5873fdb408b2f222e804d87f201bddbc
cjqian <crystaljqian@gmail.com>
2015-05-03 23:37:39 -0400

    asdf

--- events/models.py
+++ events/models.py
@@ -19,6 +19,8 @@ class Instance(models.Model):
 	timezone = models.CharField(max_length=20, default='Eastern')
 
 	is_scheduled = models.BooleanField(default='False')
+	scheduled_start = models.DateTimeField('event time')
+	scheduled_end = models.DateTimeField('event time')
 
 	def regValidate(self):
 		if len(self.title.replace(' ', '')) == 0:
