c048d94d86074cd8fdb749816261d11616f5ee77
kingroy <roy_h_zhao@yahoo.com>
2015-05-04 15:30:01 -0400

    Copy Pasta Sucks

--- beta/events/views.py
+++ beta/events/views.py
@@ -482,6 +482,7 @@ def registerEvent(request):
 	last_name = request.POST['last_name']
 	email = request.POST['username']
 	eventID = request.POST.get('eventID', '')
+	event = get_object_or_404(Instance, eventID=eventID)
 	saveInfo = {'first_name':first_name, 'last_name':last_name, 'email':email, 'event':event}
 	registered = False
 
