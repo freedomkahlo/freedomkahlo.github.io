8017fe85ce74f29a69044bb3ab89bc8a9c0792da
jl25 <jonathanlin@nat-oitwireless-inside-vapornet100-a-31543.Princeton.EDU>
2015-05-04 13:25:26 -0400

    Add eventID to register

--- beta/events/views.py
+++ beta/events/views.py
@@ -478,6 +478,7 @@ def registerEvent(request):
 	first_name = request.POST['first_name']
 	last_name = request.POST['last_name']
 	email = request.POST['username']
+	eventID = request.POST.get('eventID', '')
 	saveInfo = {'first_name':first_name, 'last_name':last_name, 'email':email}
 	registered = False
 
