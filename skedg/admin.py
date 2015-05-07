a949ff509ca1815f5d890f718a8069b08f544b40
minseungchoi <minsung0315@gmail.com>
2015-05-07 01:22:17 -0400

    admin instance changes
    df

--- skedg/admin.py
+++ skedg/admin.py
@@ -7,13 +7,20 @@ class InviteeInline(admin.TabularInline):
 class InstanceAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,	{'fields':['title']}),
+		(None,	{'fields':['creator']}),
 		(None,	{'fields':['desc']}), 
 		(None, {'fields':['start_date']}),
 		(None, {'fields':['end_date']}),
 		(None, {'fields':['start_time']}),
-		(None, {'fields':['end_time']}),]
+		(None, {'fields':['end_time']}),
+		(None, {'fields':['timezone']}),
+		(None, {'fields':['event_length']}),
+		(None, {'fields':['eventID']}),
+		(None, {'fields':['is_scheduled']}),
+		(None, {'fields':['scheduled_start']}),
+		(None, {'fields':['scheduled_end']}),]
 	inlines = [InviteeInline]
-	list_display = ('title', 'desc', 'pub_date', 'creator', 'eventID')
+	list_display = ('title', 'desc', 'pub_date', 'creator', 'eventID', 'is_scheduled')
 
 admin.site.register(Instance, InstanceAdmin)
 admin.site.register(UserProfile)
