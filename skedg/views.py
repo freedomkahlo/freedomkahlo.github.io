f069a807089d5040afe1c67bf6705ac37269b98b
kingroy <roy_h_zhao@yahoo.com>
2015-05-10 12:34:57 -0400

    delete prints

--- skedg/views.py
+++ skedg/views.py
@@ -734,8 +734,6 @@ def vetoPoss(request):
 	invitee.save()
 	possTimes = event.posstime_set.all()
 	requestTimes = [int(x) for x in request.POST.getlist('vetoTimes')]
-	print "Hiiii"
-	print request.POST.getlist('vetoTimes')
 	for pID in requestTimes:
 		p = possTimes.get(id=pID)
 		needToContinue = False
